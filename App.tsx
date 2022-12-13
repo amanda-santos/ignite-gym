5e1191364828d1479c653a7952e50909c6206171
amanda-santos <amandasf0200@gmail.com>
2022-12-13 16:57:42 -0300

    chore: setup unauthenticated routes

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import {
 import { NativeBaseProvider } from "native-base";
 
 import { Loading } from "@components/Loading";
-import { SignUp } from "@screens/SignUp";
+import { Routes } from "@routes/index";
 import { THEME } from "./src/theme";
 
 export default function App() {
@@ -20,7 +20,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignUp /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -11,7 +11,7 @@ export const SignIn = () => {
       contentContainerStyle={{ flexGrow: 1 }}
       showsVerticalScrollIndicator={false}
     >
-      <VStack flex={1} bg="gray.700" px={10} pb={16}>
+      <VStack flex={1} px={10} pb={16}>
         <Image
           source={backgroundImg}
           alt="People working out"
