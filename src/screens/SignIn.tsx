8a61b6b276000810ce24013ded52995c42dfcece
amanda-santos <amandasf0200@gmail.com>
2022-12-13 15:54:47 -0300

    feat: add logo svg on SignIn page

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,6 +1,7 @@
-import { Image, VStack } from "native-base";
+import { Center, Image, Text, VStack } from "native-base";
 
 import backgroundImg from "@assets/background.png";
+import LogoSvg from "@assets/logo.svg";
 
 export const SignIn = () => {
   return (
@@ -11,6 +12,14 @@ export const SignIn = () => {
         resizeMode="contain"
         position="absolute"
       />
+
+      <Center my={24}>
+        <LogoSvg />
+
+        <Text color="gray.100" fontSize="sm">
+          Train your mind and body.
+        </Text>
+      </Center>
     </VStack>
   );
 };
