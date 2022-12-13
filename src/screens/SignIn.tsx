18a9aadea6d61521de3f84fb4319e58cdb701cb2
amanda-santos <amandasf0200@gmail.com>
2022-12-13 16:21:24 -0300

    feat: finish Sign In page

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,25 +1,55 @@
-import { Center, Image, Text, VStack } from "native-base";
+import { Center, Heading, Image, ScrollView, Text, VStack } from "native-base";
 
 import backgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
 export const SignIn = () => {
   return (
-    <VStack flex={1} bg="gray.700">
-      <Image
-        source={backgroundImg}
-        alt="People working out"
-        resizeMode="contain"
-        position="absolute"
-      />
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bg="gray.700" px={10} pb={16}>
+        <Image
+          source={backgroundImg}
+          alt="People working out"
+          resizeMode="contain"
+          position="absolute"
+        />
 
-      <Center my={24}>
-        <LogoSvg />
+        <Center my={24}>
+          <LogoSvg />
 
-        <Text color="gray.100" fontSize="sm">
-          Train your mind and body.
-        </Text>
-      </Center>
-    </VStack>
+          <Text color="gray.100" fontSize="sm">
+            Train your mind and body
+          </Text>
+        </Center>
+
+        <Center>
+          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+            Access your account
+          </Heading>
+
+          <Input
+            placeholder="Email"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <Input placeholder="Password" secureTextEntry />
+
+          <Button title="Sign in" />
+        </Center>
+
+        <Center mt={24}>
+          <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
+            Still don't have an account?
+          </Text>
+        </Center>
+
+        <Button title="Sign up" variant="outline" />
+      </VStack>
+    </ScrollView>
   );
 };
