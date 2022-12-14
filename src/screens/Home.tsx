c5e9869f540a6cd4504f118188b37e9a4356d567
amanda-santos <amandasf0200@gmail.com>
2022-12-14 18:42:18 -0300

    feat: create exercises groups

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,40 @@
-import { VStack } from "native-base";
+import { useState } from "react";
+import { FlatList, Heading, HStack, Text, VStack } from "native-base";
 
 import { HomeHeader } from "@components/HomeHeader";
+import { Group } from "@components/Group";
 
 export const Home = () => {
+  const [groupSelected, setGroupSelected] = useState("costa");
+  const [groups, setGroups] = useState([
+    "Back",
+    "Biceps",
+    "Triceps",
+    "Shoulders",
+  ]);
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{
+          px: 8,
+        }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   );
 };
