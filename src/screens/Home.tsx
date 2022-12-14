2981cf28bcf6d9fe83b7c0a0d2d1403fcb8f1faa
amanda-santos <amandasf0200@gmail.com>
2022-12-14 18:47:24 -0300

    feat: create exercises list

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,6 +3,7 @@ import { FlatList, Heading, HStack, Text, VStack } from "native-base";
 
 import { HomeHeader } from "@components/HomeHeader";
 import { Group } from "@components/Group";
+import { ExerciseCard } from "@components/ExerciseCard";
 
 export const Home = () => {
   const [groupSelected, setGroupSelected] = useState("costa");
@@ -11,6 +12,12 @@ export const Home = () => {
     "Biceps",
     "Triceps",
     "Shoulders",
+  ]);
+  const [exercises, setExercises] = useState([
+    "Puxada frontal",
+    "Remada curvada",
+    "Remada unilateral",
+    "Levantamento terras",
   ]);
 
   return (
@@ -23,7 +30,9 @@ export const Home = () => {
         renderItem={({ item }) => (
           <Group
             name={item}
-            isActive={groupSelected === item}
+            isActive={
+              groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()
+            }
             onPress={() => setGroupSelected(item)}
           />
         )}
@@ -34,6 +43,28 @@ export const Home = () => {
         }}
         my={10}
         maxH={10}
+      />
+
+      <VStack px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercises
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            {exercises.length}
+          </Text>
+        </HStack>
+      </VStack>
+
+      <FlatList
+        data={exercises}
+        keyExtractor={(item) => item}
+        renderItem={() => <ExerciseCard />}
+        showsVerticalScrollIndicator={false}
+        _contentContainerStyle={{
+          paddingBottom: 20,
+        }}
       />
     </VStack>
   );
