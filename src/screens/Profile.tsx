21db36c8b019c0b2bcba345a9dbb4e2180833b60
amanda-santos <amandasf0200@gmail.com>
2022-12-14 19:27:43 -0300

    feat: create profile page

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,89 @@
-import { Center, Text } from "native-base";
+import { useState } from "react";
+import { TouchableOpacity } from "react-native";
+import {
+  Center,
+  ScrollView,
+  VStack,
+  Skeleton,
+  Text,
+  Heading,
+} from "native-base";
+
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+
+const PHOTO_SIZE = 33;
 
 export const Profile = () => {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
+
   return (
-    <Center flex={1}>
-      <Text>Home</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Profile" />
+
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt={6} px={10}>
+          {photoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: "https://github.com/amanda-santos.png" }}
+              alt="Foto do usuário"
+              size={PHOTO_SIZE}
+            />
+          )}
+
+          <TouchableOpacity>
+            <Text
+              color="green.500"
+              fontWeight="bold"
+              fontSize="md"
+              mt={2}
+              mb={8}
+            >
+              Update profile picture
+            </Text>
+          </TouchableOpacity>
+
+          <Input bg="gray.600" placeholder="Name" />
+
+          <Input bg="gray.600" placeholder="Email" isDisabled />
+
+          <Heading
+            color="gray.200"
+            fontSize="md"
+            mb={2}
+            alignSelf="flex-start"
+            mt={12}
+          >
+            Change password
+          </Heading>
+
+          <Input
+            bg="gray.600"
+            placeholder="Previous password"
+            secureTextEntry
+          />
+
+          <Input bg="gray.600" placeholder="New password" secureTextEntry />
+
+          <Input
+            bg="gray.600"
+            placeholder="Confirm your new password"
+            secureTextEntry
+          />
+
+          <Button title="Save" mt={4} />
+        </Center>
+      </ScrollView>
+    </VStack>
   );
 };
