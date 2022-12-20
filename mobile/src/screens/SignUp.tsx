6dd0778dfd7c501bbc60b4b34726d3d411195d8e
amanda-santos <amandasf0200@gmail.com>
2022-12-20 19:17:03 -0300

    refactor: handle api errors with toast and interceptor

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -1,10 +1,16 @@
-import { Alert } from "react-native";
-import { Center, Heading, Image, ScrollView, Text, VStack } from "native-base";
+import {
+  Center,
+  Heading,
+  Image,
+  ScrollView,
+  Text,
+  useToast,
+  VStack,
+} from "native-base";
 import { useNavigation } from "@react-navigation/native";
 import { useForm, Controller } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
-import axios from "axios";
 
 import backgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg";
@@ -13,6 +19,7 @@ import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
 import { api } from "@services/api";
+import { AppError } from "@utils/AppError";
 
 type FormDataProps = {
   name: string;
@@ -35,6 +42,8 @@ const signUpSchema = yup.object({
 });
 
 export const SignUp = () => {
+  const toast = useToast();
+
   const {
     control,
     handleSubmit,
@@ -54,9 +63,17 @@ export const SignUp = () => {
       const response = await api.post("/users", { name, email, password });
       console.log(response);
     } catch (error) {
-      if (axios.isAxiosError(error)) {
-        Alert.alert("Erro", error.response?.data.message);
-      }
+      const isAppError = error instanceof AppError;
+
+      const title = isAppError
+        ? error.message
+        : "Não foi possível criar a conta. Tente novamente mais tarde.";
+
+      toast.show({
+        title,
+        placement: "top",
+        bgColor: "red.500",
+      });
     }
   };
 
