bb8d9663d3fdb814c50e62cabfba9e4bbbd3fcf0
amanda-santos <amandasf0200@gmail.com>
2022-12-22 16:03:00 -0300

    feat: redirect user to app on sign in

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,11 +1,14 @@
 import { useTheme, Box } from "native-base";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
+import { useAuth } from "@hooks/useAuth";
+
 import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
 
 export const Routes = () => {
   const { colors } = useTheme();
+  const { user } = useAuth();
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
@@ -13,7 +16,7 @@ export const Routes = () => {
   return (
     <Box flex={1} bg="gray.700">
       <NavigationContainer>
-        <AuthRoutes />
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   );
